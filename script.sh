9cb9c06e104a6c8ef53aad5bc452f29e14cea307
Pedro Henrique Clain <pedroclain20@gmail.com>
2021-11-23 14:37:00 -0300

    mudei

--- script.sh
+++ script.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-
 dirDrive=~/backup/google-drive
-
+fusermount -u $dirDrive
+echo "Montando drive com google ocamlfuse..."
 google-drive-ocamlfuse $dirDrive
-
-rsync -avh ~/Documents/pedro $dirDrive --delete
-
+sleep 5
+cd $dirDrive
+echo "Apagando backup antigo..."
+rm backup.tar.gz
+cd ~/backup
+echo "Comprimindo arquivos..."
+tar cvzf backup.tar.gz ~/pasta/backup
+echo "Enviando para o drive..."
+mv backup.tar.gz $dirDrive
 echo "Backup realizado com sucesso!"
